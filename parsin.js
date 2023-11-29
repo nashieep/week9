3026823fb05ac3070c01ff96858cda1ae0c73430
nashieep <nmpongnon@gmail.com>
2023-11-29 09:42:23 -0600

    strings

---
+++ parsin.js
@@ -0,0 +1,9 @@
+let str = `{"title" : "Web Designer",
+"startDate" : "October 2022",
+"company" : "AT&T",
+"minSalary" : 52000,
+"maxSalary" : 86000
+}`;
+let job = JSON.parse(str);
+console.log(job.title);
+console.log(job.company);
--- string_example.js
+++ string_example.js
@@ -1,3 +1,13 @@
+// let job = {
+//     title : "Web Designer",
+//     startDate : "October 2022",
+//     company : "AT&T",
+//     minSalary : 52000,
+//     maxSalary : 86000
+//     };
+//     let str = JSON.stringify(job);
+//     console.log(str);
+
 let job = {
     title : "Web Designer",
     startDate : "October 2022",
@@ -5,5 +15,6 @@ let job = {
     minSalary : 52000,
     maxSalary : 86000
     };
-    let str = JSON.stringify(job);
+    let filter = ["title", "company"];
+    let str = JSON.stringify(job, filter);
     console.log(str);
